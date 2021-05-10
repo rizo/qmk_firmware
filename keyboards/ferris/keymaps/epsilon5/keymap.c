9cd61c003157da70db316e0b2981033c4d565de9
Rizo I <rizo@odis.io>
2021-05-10 20:25:26 +0100

    WIP switcher.

--- keyboards/ferris/keymaps/epsilon5/keymap.c
+++ keyboards/ferris/keymaps/epsilon5/keymap.c
@@ -14,7 +14,9 @@ enum layers {
 enum keycodes {
     _COM_SCL = SAFE_RANGE,
     _DOT_COL,
-    _EXC_QST
+    _EXC_QST,
+
+    _GUI_TAB // GUI Tab switcher
 };
 
 
@@ -34,7 +36,7 @@ enum keycodes {
 #define _ACT_TAB  LT(_ACT, KC_TAB)
 #define _NUM_ESC  LT(_NUM, KC_ESC)
 #define _SYM_ENT  LT(_SYM, KC_ENT)
-#define _ACT_SPC  LT(_ACT, KC_SPC)
+#define _NUM_BSP  LT(_NUM, KC_BSPC)
 
 // ACT keys
 #define _UNDO G(KC_Z)
@@ -46,6 +48,8 @@ enum keycodes {
 #define  _VOLD KC__VOLDOWN
 #define  _MUTE KC__MUTE
 
+#define _SFT_ESC SFT_T(KC_ESC)
+
 
 // Keymap
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -53,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_F,     KC_P,     KC_G,                          KC_J,     KC_L,     KC_U,     KC_Y,  KC_QUOT,
           _A,      _R,      _S,       _T,     KC_D,                          KC_H,       _N,       _E,       _I,       _O,
         KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,                          KC_K,     KC_M, _COM_SCL, _DOT_COL, _EXC_QST,
-                                          _ACT_TAB, _NUM_ESC,  _SYM_ENT, _ACT_SPC
+                                          _ACT_TAB, _NUM_ESC,  _SYM_ENT, _NUM_BSP
   ),
 
   [_SYM] = LAYOUT(
@@ -71,12 +75,79 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_ACT] = LAYOUT(
-      XXXXXXX, KC_MPRV, KC_MNXT, KC_MPLY, XXXXXXX,                   KC_PGUP,   _UNDO,   KC_UP,   _REDO, KC_HOME,
-      KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, XXXXXXX,                   KC_PGDN, KC_LEFT, KC_DOWN,KC_RIGHT,  KC_END,
-      XXXXXXX,   _VOLD,   _VOLU,   _MUTE, XXXXXXX,                   XXXXXXX,   _COPY,   _PAST,    _CUT, XXXXXXX,
-                                          _______, _______,  KC_EQL, KC_BSPC
+      XXXXXXX, KC_MPRV,  KC_MNXT,  KC_MPLY, XXXXXXX,                   KC_PGUP,   _UNDO,   KC_UP,   _REDO, KC_HOME,
+      KC_LCTL, KC_LALT, _GUI_TAB, _SFT_ESC, XXXXXXX,                   KC_PGDN, KC_LEFT, KC_DOWN,KC_RIGHT,  KC_END,
+      XXXXXXX,   _VOLD,    _VOLU,    _MUTE, XXXXXXX,                   XXXXXXX,   _COPY,   _PAST,    _CUT, XXXXXXX,
+                                            _______, _______,  KC_EQL, KC_BSPC
   )
 };
+
+
+// Swapper
+
+bool _s_app_active = false;
+
+bool switch_app(bool *active, uint16_t keycode, keyrecord_t *record) {
+  // Activate swapper.
+  if (keycode == _GUI_TAB) {
+    if (record->event.pressed) {
+      if (!*active) {
+        *active = true;
+        register_code(KC_LGUI);
+        register_code(KC_TAB);
+        return false;
+      } else {
+        register_code(KC_TAB);
+        return false;
+      }
+    } else { // released
+      // Don't unregister KC_LGUI yet.
+      unregister_code(KC_TAB);
+      return false;
+    }
+  } 
+  // Release the layer key.
+  else if (keycode == _ACT_TAB && !record->event.pressed) {
+    unregister_code(KC_LGUI);
+    *active = false;
+    return true;
+  }
+  // Terminate on ESC.
+  else if (*active && (keycode == KC_ESC && record->event.pressed)) {
+    *active = false;
+    return true;
+  } else {
+    return true;
+  }
+}
+
+
+bool switch_tab(uint16_t keycode, keyrecord_t *record) {
+  if (keycode == _K_S_TAB) {
+    if (record->event.pressed) {
+      tap_code16(get_mods() & MOD_BIT(KC_LSHIFT) ? G(S(KC_LBRC)) : G(S(KC_RBRC)));
+    }
+    return false;
+  } else {
+    return true;
+  }
+}
+
+bool switch_spl(uint16_t keycode, keyrecord_t *record) {
+  if (keycode == _K_S_SPL) {
+    if (record->event.pressed) {
+      if (get_mods() & MOD_BIT(KC_LSHIFT)) {
+        unregister_code(KC_LSHIFT);
+        tap_code16(G(KC_LBRC));
+      } else {
+        tap_code16(G(KC_RBRC));
+      }
+    }
+    return false;
+  } else {
+    return true;
+  }
+}
 
 
 // Function overrides.
@@ -87,6 +158,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   bool is_alt_on = (get_mods() & MOD_BIT(KC_LALT));
   bool is_sft_on = (get_mods() & MOD_BIT(KC_LSHIFT));
   bool is_ctl_on = (get_mods() & MOD_BIT(KC_LCTL));
+
+
+  // Switcher
+  if (IS_LAYER_ON(_ACT)) {
+    ret = switch_app(&_s_app_active, keycode, record);
+  }
 
 
   // '.' / ':'
