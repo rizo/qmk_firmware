5dcd85c7b4b16c4b670b853f1c32a824c809b654
Rizo Isrof <rizo.isrof@gmail.com>
2021-05-20 12:19:56 +0100

    Eps 5.3 sft.

--- keyboards/ferris/keymaps/epsilon5/config.h
+++ keyboards/ferris/keymaps/epsilon5/config.h
@@ -8,7 +8,7 @@
 #define TAPPING_TERM 200
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
+#define IGNORE_MOD_TAP_INTERRUPT_PER_KEY
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
--- keyboards/ferris/keymaps/epsilon5/keymap.c
+++ keyboards/ferris/keymaps/epsilon5/keymap.c
@@ -48,10 +48,10 @@ enum keycodes {
 
 
 // Thumb keys
-#define _ACT_SPC  LT(_ACT, KC_SPC)
-#define _NUM_TAB  LT(_NUM, KC_TAB)
+#define _ACT_BSP  LT(_ACT, KC_BSPC)
+#define _SFT_TAB  SFT_T(KC_TAB)
 #define _SYM_ENT  LT(_SYM, KC_ENT)
-#define _FUN_BSP  LT(_FUN, KC_BSPC)
+#define _NUM_SPC  LT(_NUM, KC_SPC)
 
 // ACT keys
 #define _UNDO G(KC_Z)
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_F,     KC_P,     KC_G,                          KC_J,     KC_L,     KC_U,     KC_Y,  KC_QUOT,
           _A,      _R,      _S,       _T,     KC_D,                          KC_H,       _N,       _E,       _I,       _O,
         KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,                          KC_K,     KC_M, _COM_SCL, _DOT_COL, _EXC_QST,
-                                          _ACT_SPC, _NUM_TAB,  _SYM_ENT, _FUN_BSP
+                                          _ACT_BSP, _SFT_TAB,  _SYM_ENT, _NUM_SPC
   ),
 
   [_SYM] = LAYOUT(
@@ -88,24 +88,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_NUM] = LAYOUT(
-      XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,                   KC_PLUS, KC_1, KC_2, KC_3, KC_CIRC,
-      KC_LCTL, KC_LALT, KC_LGUI,  KC_LSFT, XXXXXXX,                   KC_MINS, KC_4, KC_5, KC_6, KC_0,
-      XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,                   KC_ASTR, KC_7, KC_8, KC_9, KC_SLSH,
-                                          XXXXXXX, _______,  KC_EQL, KC_UNDS
+      KC_CIRC, KC_1, KC_2, KC_3, KC_ASTR,                    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,
+       KC_EQL, KC_4, KC_5, KC_6, KC_MINS,                    XXXXXXX, KC_LSFT, KC_LGUI,  KC_LALT, KC_LCTL,
+      KC_SLSH, KC_7, KC_8, KC_9, KC_PLUS,                    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,
+                                    KC_0, KC_UNDS,  XXXXXXX, _______
   ),
 
   [_ACT] = LAYOUT(
      KC_ESC,  G(KC_W),G(KC_LBRC),G(KC_RBRC), _UNREDO,                    KC_PGUP, A(KC_LEFT),   KC_UP, A(KC_RIGHT), G(KC_UP),
    _WIN_SWP, _APP_SWP,  _SPL_SWP,  _TAB_SWP, G(KC_A),                    KC_PGDN,    KC_LEFT, KC_DOWN,    KC_RIGHT, G(KC_DOWN),
       _UNDO,     _CUT,     _COPY,     _PAST,   _REDO,                    XXXXXXX, G(KC_LEFT), KC_CAPS, G(KC_RIGHT), XXXXXXX,
-                                              _______, XXXXXXX,  KC_ESC, KC_BSPC
+                                              _______, XXXXXXX,  KC_ENT, KC_ESC
   ),
 
   [_FUN] = LAYOUT(
        KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_EJCT,                   XXXXXXX,   _MUTE,   _VOLD,    _VOLU, XXXXXXX,
        KC_F5,  KC_F6,  KC_F7,  KC_F8,   KC_CLR,                   XXXXXXX, KC_LSFT, KC_LGUI,  KC_LALT, KC_LCTL,
        KC_F9,  KC_F10, KC_F11, KC_F12, XXXXXXX,                   XXXXXXX, KC_MPLY, KC_MPRV,  KC_MNXT, XXXXXXX,
-                                       XXXXXXX, XXXXXXX,  XXXXXXX, _______
+                                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
   )
 };
 
@@ -156,7 +156,7 @@ bool switch_app(bool *active, uint16_t keycode, keyrecord_t *record) {
   }
 
   // Unregister KC_LGUI on layer release.
-  else if (keycode == _ACT_SPC && !record->event.pressed) {
+  else if (keycode == _ACT_BSP && !record->event.pressed) {
     unregister_code(KC_LGUI);
     *active = false;
     return true;
@@ -194,7 +194,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
 
   // SFT BSP to DEL
-  if (((keycode == _FUN_BSP && record->tap.count > 0) || keycode == KC_BSPC) && record->event.pressed) {
+  if (((keycode == _ACT_BSP && record->tap.count > 0) || keycode == KC_BSPC) && record->event.pressed) {
     if (is_sft_on) {
       if (is_gui_on) {
         unregister_code(KC_LSHIFT);
@@ -258,13 +258,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       register_code(KC_LSHIFT);
     }
 
-
     ret = false;
   }
 
   // ACT: WIN SWP tap action
   else if (keycode == _WIN_SWP && record->event.pressed && record->tap.count > 0) {
     tap_code16(G(KC_GRV));
+    ret = false;
+  }
+
+  // ABC: SFT(_NUM_SPC) -> UNDS
+  else if (keycode == _NUM_SPC && is_sft_on && record->event.pressed && record->tap.count > 0) {
+    tap_code16(KC_UNDS);
     ret = false;
   }
 
@@ -334,4 +339,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
 
   return ret;
+}
+
+
+bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case _SFT_TAB:
+      return false;
+    case _NUM_SPC:
+      return false;
+    case _SYM_ENT:
+      return false;
+    case _ACT_BSP:
+      return false;
+    
+    default:
+      return true;
+  }
 }
