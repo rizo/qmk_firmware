ca8c75fcb07ff455e9224e383bdc59f258e75d21
Rizo I <rizo@odis.io>
2024-07-02 15:25:18 +0100

    eps10

--- keyboards/ferris/keymaps/epsilon10/keymap.c
+++ keyboards/ferris/keymaps/epsilon10/keymap.c
@@ -149,7 +149,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     EP_PIPB, EP_PIPF, EP_ARR1, EP_ARR2, EP_HOMD,                      KC_PLUS, KC_1,    KC_2,    KC_3,    KC_CIRC,
     KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, EP_CURD,                      KC_MINS, KC_4,    KC_5,    KC_6,    KC_0,
     XXXXXXX,  EP_GBP, EP_CEDL,  EP_EUR, EP_PARD,                      KC_ASTR, KC_7,    KC_8,    KC_9,    KC_SLSH,
-                                        XXXXXXX, _______,    KC_EQL,  KC_UNDS
+                                        XXXXXXX, _______,    KC_EQL,  KC_SPC
   ),
 
   [SYM] = LAYOUT(
